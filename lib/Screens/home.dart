5a0929da757f32184cdeb0d04ed6009f1fd61972
Vincent K. Kogei <42339869+mervinkogei@users.noreply.github.com>
2024-04-02 19:27:20 +0300

    feat: Added featured products

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:barizi/Utils/data_function.dart';
+import 'package:barizi/Utils/dummy_data.dart';
 import 'package:barizi/Utils/textStyling.dart';
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
@@ -13,12 +14,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
 final controller = PageController(viewportFraction: 0.8, keepPage: true);
 /// List of the products categories
-List productsMenu = [
-  {"name": "Women", "image": "images/women.png"},
-  {"name": "Men", "image": "images/men.png"},
-  {"name": "Accessories", "image": "images/accessories.png"},
-  {"name": "Beauty", "image": "images/beauty.png"},
-  ];
+
 
 
   @override
@@ -54,94 +50,126 @@ List productsMenu = [
       appBar: DataFunction.menuAppBar(context, 'GemStore'),
       drawer: const Drawer(),
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-
-      /**
-       * Here is the list of items to Select the categories
-       */
-       Container(
-          margin: const EdgeInsets.symmetric(vertical: 20),
-          height: 100,
-          child: ListView.builder(
-            itemCount: productsMenu.length,
-            scrollDirection: Axis.horizontal,
-            shrinkWrap: true,
-            itemBuilder: (context, index){
-              return InkWell(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    width: 90,
-                    child: Column(
-                      children: [
-                        Container(
-                          height: 45,width: 45,
-                          decoration: BoxDecoration(
-                            color:  index == 0 ? Colors.black:  Theme.of(context).focusColor ,
-                            borderRadius: BorderRadius.circular(20)
-                          ),
-                          child: Image.asset(productsMenu[index]["image"])),
-                        const SizedBox(height: 5,),
-                        Text(productsMenu[index]["name"]),
-                      ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+        
+        /**
+         * Here is the list of items to Select the categories
+         */
+         Container(
+            margin: const EdgeInsets.symmetric(vertical: 20),
+            height: 100,
+            child: ListView.builder(
+              itemCount: productsMenu.length,
+              scrollDirection: Axis.horizontal,
+              shrinkWrap: true,
+              itemBuilder: (context, index){
+                return InkWell(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: 90,
+                      child: Column(
+                        children: [
+                          Container(
+                            height: 45,width: 45,
+                            decoration: BoxDecoration(
+                              color:  index == 0 ? Colors.black:  Theme.of(context).focusColor ,
+                              borderRadius: BorderRadius.circular(20)
+                            ),
+                            child: Image.asset(productsMenu[index]["image"])),
+                          const SizedBox(height: 5,),
+                          Text(productsMenu[index]["name"]),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              );
-            },
-            
+                );
+              },
+              
+            ),
           ),
-        ),
-
-        /**
-         * Here is the Carousel banner with indicators to dynamically show the carousel items
-         */
         
-        Column(
-          children: [
-             SizedBox(
-                height: 180,
-                child: PageView.builder(
-                  controller: controller,
-                  itemBuilder: (_, index) {
-                    return Column(
-                      children: [
-                        pages[index % pages.length],
-                         SmoothPageIndicator(
-                          controller: controller,
-                          count: pages.length,
-                          effect: const WormEffect(
-                            dotHeight: 10,
-                            dotWidth: 10,
-                            type: WormType.normal,
-                          ),
-                        ),
-                      ],
-                    );
-                  },
-                ),
-              ),
-                         
-          ],
-        ),
-
-        /**
-         * Featured Products Section Starts here
-         */
-
-        const Padding(
-          padding: EdgeInsets.symmetric(horizontal: 15),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          /**
+           * Here is the Carousel banner with indicators to dynamically show the carousel items
+           */
+          
+          Column(
             children: [
-              Text('Feature Products', style: ThemeStyling.featuredText,),
-              Text('Show all', style: ThemeStyling.showAllText,),
+               SizedBox(
+                  height: 180,
+                  child: PageView.builder(
+                    controller: controller,
+                    itemBuilder: (_, index) {
+                      return Column(
+                        children: [
+                          pages[index % pages.length],
+                           SmoothPageIndicator(
+                            controller: controller,
+                            count: pages.length,
+                            effect: const WormEffect(
+                              dotHeight: 10,
+                              dotWidth: 10,
+                              type: WormType.normal,
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                ),
+                           
             ],
           ),
-        )
-
-        ],
+        
+          /**
+           * Featured Products Section Starts here
+           */
+        
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('Feature Products', style: ThemeStyling.featuredText,),
+                Text('Show all', style: ThemeStyling.showAllText,),
+              ],
+            ),
+          ),
+        
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 20),
+            height: 200,
+            child: ListView.builder(
+              itemCount: products.length,
+              scrollDirection: Axis.horizontal,
+              shrinkWrap: true,
+              itemBuilder: (context, index){
+                return InkWell(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      width: 90,
+                      child: Column(
+                        children: [
+                          Container(
+                            height: 126,width: 192,
+                            child: Image.asset(products[index]["image"])),
+                          const SizedBox(height: 5,),
+                          Text(products[index]["product"]),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+              
+            ),
+          ),
+        
+          ],
+        ),
       ),
     );
   }
---
+++ lib/Utils/dummy_data.dart
@@ -0,0 +1,15 @@
+
+  List productsMenu = [
+  {"name": "Women", "image": "images/women.png"},
+  {"name": "Men", "image": "images/men.png"},
+  {"name": "Accessories", "image": "images/accessories.png"},
+  {"name": "Beauty", "image": "images/beauty.png"},
+  ];
+
+  List products = [
+  {"product": "Turtleneck Sweater", "image": "images/product_1.png", "price": "\$ 39.99"},
+  {"product": "Long Sleeve Dress", "image": "images/product_2.png", "price": "\$ 45.00"},
+  {"product": "Sportwear Set", "image": "images/product_1.png", "price": "\$ 89.99"},
+  {"product": "Sportwear Set", "image": "images/product_2.png", "price": "\$ 42.99"},
+  ];
+
